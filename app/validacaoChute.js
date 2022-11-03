e326982061c06efd25bdf80314ca99275281b210
gabrielnvitor <gabrielnvitor@gmail.com>
2022-11-03 12:10:17 -0300

    validação e reconhecimento de Voz concluidas

---
+++ app/validacaoChute.js
@@ -0,0 +1,34 @@
+function verificaChuteComValorValido(chute){
+    const numero = +chute;
+
+    if(chuteInvalido(numero)){
+        elementoChute.innerHTML += `<div>Número inválido</div>`
+        return
+    }
+
+    if(numeroMaiorOuMenorQuePermitido(numero)){
+        elementoChute.innerHTML += `<div>Número inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = 
+        `<h2>Você acertou</h2>
+        <h3>O número secreto é ${numeroSecreto}</h3>
+        `
+        return
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += '<div>O número secreto é menor <i class="fa-solid fa-circle-chevron-down"></i></div>'
+    } else{
+        elementoChute.innerHTML += '<div>O número secreto é maior <i class="fa-solid fa-circle-chevron-up"></i></div>'
+    }
+    
+    function chuteInvalido() {
+        return Number.isNaN(numero);
+    }
+
+    function numeroMaiorOuMenorQuePermitido(numero){
+        return numero > maiorValor || numero < menorValor
+    }
+
+}
